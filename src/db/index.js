4eaa2dea03fe17b0d23ed156501481423d006d32
aahwarg <aahan.wargane@gmail.com>
2025-07-10 13:21:31 +0530

    mongoDB

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import { DB_NAME } from '../constants.js';
+import dotenv from 'dotenv';
+
+dotenv.config(); // <- load environment variables here
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+    console.log(`✅ DB connected — HOST: ${connectionInstance.connection.host}`);
+  } catch (error) {
+    console.log('❌ DB connection error:', error);
+    throw error;
+  }
+};
+
+export default connectDB;
